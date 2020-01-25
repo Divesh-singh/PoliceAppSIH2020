a8b8896b68a85be785740dff659c0773252348ac
sahyog saini <42652212+thephenomenal10@users.noreply.github.com>
2020-01-26 00:13:38 +0530

    Drawer navigation and bottom tab navigation bar styling added

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Dimensions} from 'react-native';
+import {View,StyleSheet, Dimensions} from 'react-native';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import { createDrawerNavigator} from 'react-navigation-drawer';
 import {createStackNavigator} from 'react-navigation-stack';
@@ -7,6 +7,7 @@ import {createBottomTabNavigator} from 'react-navigation-tabs';
 import {Ionicons} from '@expo/vector-icons';
 import {AntDesign} from '@expo/vector-icons';
 import {Octicons} from '@expo/vector-icons';
+// import FirebaseKeys from './config/config';
 
 
 import LoadingScreen from './screens/LoadingScreen';
@@ -15,27 +16,26 @@ import HomeScreen from './screens/HomeScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import NotificationScreen from './screens/NotificationScreen';
 import ScannerScreen from './screens/ScannerScreen';
-import VerificationScreen from './screens/VerificationScreen';
-import BeatManagementScreen from './screens/BeatManagementScreen';
-import CrimeRecordScreen from './screens/CrimeRecordScreen';
 import HeatMapScreen from './screens/HeatMapScreen';
-
-import FirebaseKeys from './config/config';
+import VerificationScreen from './screens/VerificationScreen';
 
 
 import * as firebase from 'firebase';
 
-var firebaseConfig = FirebaseKeys;
+var firebaseConfig = {
+  apiKey: "AIzaSyAf79sr9bJjAHtQpJyQa7DVVNZvjwZ1zgQ",
+  authDomain: "sih2020-42ea1.firebaseapp.com",
+  databaseURL: "https://sih2020-42ea1.firebaseio.com",
+  projectId: "sih2020-42ea1",
+  storageBucket: "sih2020-42ea1.appspot.com",
+  messagingSenderId: "43086957866",
+  appId: "1:43086957866:web:87d03b215fb73387816253"
+}
 // Initialize Firebase
 if(! firebase.apps.length){
-   firebase.initializeApp({firebaseConfig});
+  firebase.initializeApp(firebaseConfig);
 }
 
-// const AppStack = createStackNavigator({
-//   Home: HomeScreen,
-//   Crime: CrimeRecordScreen,
-//   Beat: BeatManagementScreen, 
-// })
 
 
 const width = Dimensions.get("window").width;
@@ -44,6 +44,7 @@ const DrawerConfig = {
 };
 const Drawer = createDrawerNavigator({
   Home:HomeScreen,
+  Notification:NotificationScreen 
 },
 {
   headerMode: "none"
@@ -57,14 +58,22 @@ const AppTabNavigator = createBottomTabNavigator({
  Drawer:Drawer,
   Home:{
     screen: HomeScreen,
+    
     navigationOptions:{
-      tabBarIcon: ({tintColor}) => <Ionicons name = "ios-home" size={24} color={tintColor}/>
+      tabBarIcon: ({tintColor}) => 
+      <View style = {styles.bottomIcon1}>
+      <Ionicons name = "ios-home" size={24} color={tintColor} />
+     </View>
     }
+    
   },
   Ver:{
     screen: VerificationScreen,
     navigationOptions:{
-      tabBarIcon: ({tintColor}) => <Octicons name = "verified" size={24} color={tintColor}/>
+      tabBarIcon: ({tintColor}) => 
+      <View style={styles.bottomIcon2}>
+      <Octicons name = "verified" size={24} color={tintColor}/>
+      </View>
     }
   },
 
@@ -72,17 +81,20 @@ const AppTabNavigator = createBottomTabNavigator({
   Qrscanner:{
     screen: ScannerScreen,
     navigationOptions:{
-      tabBarIcon: ({tintColor}) => <AntDesign 
+      tabBarIcon: ({tintColor}) =>
+      <View style={styles.bottomIcon3}>
+      <AntDesign 
       name = "qrcode" 
-      size={48} 
+      size={49} 
       color= "#E9446A"
       style={{
           shadowColor: "#E9446A",
           shadowOffset: {width:0, height:0},
-          shadowRadius: 40,
+          shadowRadius: 100,
           shadowOpacity: 10
       }}
       />
+      </View>
     }
   },
   Notification:{
@@ -105,49 +117,46 @@ const AppTabNavigator = createBottomTabNavigator({
    inactiveTintColor: "#BBBBC4",
    showLabel: false
  }
-},
-
-{
-  headerMode:'none',
 }
 )
 
 
-const screenStack = createStackNavigator({
-  
-  App: AppTabNavigator,
-  Home: HomeScreen,
-  Beat: BeatManagementScreen,
-  Crime: CrimeRecordScreen,
-  Heat: HeatMapScreen,
-},
-{
-  headerMode:'none'
-});
+
 
 
 const AuthStack = createStackNavigator({
   Login: LoginScreen,
   Register: RegisterScreen
 
-},
-{
-  headerMode:'none'
 });
-
 
 export default createAppContainer(
   createSwitchNavigator(
     {
       Loading: LoadingScreen,
       App: AppTabNavigator,
-      Auth: AuthStack,
-      screen: screenStack
+      Auth: AuthStack
     },
     {
       initialRouteName:"Loading",
-      headerMode:'none'
     }
     
   )
 )
+
+
+
+//.....................adjusting a botton tab tab bar icon styling.............................
+
+styles = StyleSheet.create({
+  bottomIcon1:{
+      marginRight:51
+  },
+  bottomIcon2:{
+    marginRight:45
+},
+bottomIcon3:{
+  marginRight:24
+},
+
+})
